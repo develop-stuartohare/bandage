aeb7b2d824513dcfe1115fcd6268a6b73aa5c0d1
Stuart O'Hare <develop@stuartohare.com>
2019-06-01 15:04:01 +0100

    adding react-navigation

---
+++ app/screens/Dashboard.screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const DashboardScreen = () => {
+    return (
+        <View style={{ marginTop: 100 }}>
+            <Text> DashboardScreen</Text>
+        </View>
+    );
+};
+
+export default DashboardScreen;
---
+++ app/screens/Files.screen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const FilesScreen = () => {
+    return (
+        <View style={{ marginTop: 100 }}>
+            <Text> FilesScreen</Text>
+        </View>
+    );
+};
+
+export default FilesScreen;
